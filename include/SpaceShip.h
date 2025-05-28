cd50f2251e3a4d14ede22a7f527a0e04cb49086a
Perea094 <181926373+Perea094@users.noreply.github.com>
2025-05-28 12:18:53 -0600

    Update SpaceShip.h

---
+++ include/SpaceShip.h
@@ -0,0 +1,36 @@
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+class SpaceShip
+{
+private:
+    float fuel;
+    float cargoCapacity;
+    float efficiency;
+
+public:
+    // Constructors
+    SpaceShip();
+    SpaceShip(float fuel, float cargoCapacity, float efficiency)
+        : fuel(fuel), cargoCapacity(cargoCapacity), efficiency(efficiency) {}
+    
+    // Getters
+    float getFuel() const;
+    float getCargoCapacity() const;
+    float getEfficiency() const;
+
+    // Setters
+    void setFuel(float fuel);
+    void setCargoCapacity(float cargoCapacity);
+    void setEfficiency(float efficiency);
+    
+    
+};
+
+
+
+
+
+
+
+#endif // SPACESHIP_H
