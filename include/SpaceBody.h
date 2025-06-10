50675645f6244c5966af2620c6a096cec5c802b7
Perea094 <181926373+Perea094@users.noreply.github.com>
2025-06-10 15:39:04 -0600

    add
    apcebody and spaceship adds

--- include/SpaceBody.h
+++ include/SpaceBody.h
@@ -2,7 +2,7 @@
 #define SPACEBODY_H
 
 #include <string>
-#include "include/Resource.h"
+#include "Resource.h"
 #include <vector>
 
 class SpaceBody
@@ -16,7 +16,7 @@ protected:
 
 public:
     // Constructors
-    SpaceBody(/* args */);
+    SpaceBody();
     SpaceBody(const std::string& name, float distanceFromEarth, float radius, float mass, 
               const std::vector<Resource>& resources = {});
     
@@ -37,9 +37,12 @@ public:
     // Methods
     void operator+ (const Resource& resource); // Adds a resource to the space body
     void operator- (const Resource& resource); // Removes a resource from the space body
-    virtual std::vector<std::string> extractResources() const = 0; // Extracts resources from the space body
+    virtual std::vector<std::string> extractResources() const = 0; //
+    float getValue() const; // Returns the value of the space body
+    float getValue(float cargoCapacity);
+    virtual float getComplexity() const = 0;
     virtual std::string getInfo() const = 0; // Returns a string with information about the space body
-
+    
 
 
 };
--- include/SpaceShip.h
+++ include/SpaceShip.h
@@ -1,27 +1,36 @@
 #ifndef SPACESHIP_H
 #define SPACESHIP_H
 
+#include <string>
+
 class SpaceShip
 {
 private:
     float fuel;
-    float cargoCapacity;
+    float cargoCapacity; //in kg
     float efficiency;
+    float value;
 
 public:
     // Constructors
     SpaceShip();
-    SpaceShip(float fuel, float cargoCapacity, float efficiency);
+    SpaceShip(float fuel, float cargoCapacity, float efficiency, float value);
     
     // Getters
     float getFuel() const;
     float getCargoCapacity() const;
     float getEfficiency() const;
+    float getValue() const;
 
     // Setters
     void setFuel(float newFuel);
     void setCargoCapacity(float newCargoCapacity);
     void setEfficiency(float newEfficiency);    
+    void setValue(float newValue);
+
+    // Methods
+    std::string getInfo() const; // Returns a string with information about the spaceship
+    
 };
 
 
